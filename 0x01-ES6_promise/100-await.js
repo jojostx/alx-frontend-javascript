e6dad8d48c3382143b394441972bd40ac6e389b7
jojostx <ikuskid7@gmail.com>
2023-06-28 18:07:28 +0100

    advanced correction 1

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,7 +4,7 @@ export default async function asyncUploadUser() {
   const uploadPromise = uploadPhoto('photo-profile-1');
   const userPromise = createUser('Guillaume', 'Salva');
 
-  return await Promise.allSettled([uploadPromise, userPromise])
+  return Promise.allSettled([uploadPromise, userPromise])
     .then((results) => ({ photo: results[0].value, user: results[1].value }))
     .catch(() => ({ photo: null, user: null }));
 }

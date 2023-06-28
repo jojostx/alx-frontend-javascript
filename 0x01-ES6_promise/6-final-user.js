0da9e52e978bef40c69c110100e5071abb0274da
jojostx <ikuskid7@gmail.com>
2023-06-28 17:23:19 +0100

    correction 4

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,10 +5,11 @@ export default async function handleProfileSignup(firstName, lastName, filename)
   const p1 = signUpUser(firstName, lastName);
   const p2 = uploadPhoto(filename);
 
-  const p3 = Promise.allSettled([
-    p1.catch((e) => `${e.name}: ${e.message}`),
-    p2.catch((e) => `${e.name}: ${e.message}`),
-  ]).then((values) => values);
+  const p3 = Promise.allSettled([p1, p2])
+    .then((results) => results.map((res) => ({
+      status: res.status,
+      value: res.value || `${res.reason.name}: ${res.reason.message}`,
+    })));
 
   return p3;
 }

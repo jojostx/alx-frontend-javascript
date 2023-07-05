5e7112f116394a1e071740566e0fef319bbdfbe8
Ari Jay <ikuskid7@gmail.com>
2023-07-05 11:06:05 +0100

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,17 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
+    return '';
+  }
 
   const strings = [];
+
   set.forEach((val) => {
     if (val.startsWith(startString)) strings.push(val.replace(startString, ''));
   });
+
   return strings.join('-');
 }
